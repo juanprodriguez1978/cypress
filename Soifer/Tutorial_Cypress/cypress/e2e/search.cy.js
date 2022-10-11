d23329f4cd4ea78064de0a4677da2d22325d0dfb
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2022-10-11 20:29:41 -0300

    borrado de espacios

--- Soifer/Tutorial_Cypress/cypress/e2e/search.cy.js
+++ Soifer/Tutorial_Cypress/cypress/e2e/search.cy.js
@@ -4,9 +4,6 @@ describe('Search test cases',function(){
         cy.get('#search_query_top').type('dress')
         cy.get('#searchbox > .btn').click()
         cy.get('.lighter').contains('"dress"')
-
-
-
-
     })
+    
 })
