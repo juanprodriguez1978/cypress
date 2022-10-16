803fcc6a1fee3af310796c39cd8f33870d91c273
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2022-10-16 17:07:48 -0300

    change file

--- Soifer/Tutorial_Cypress/cypress/e2e/each.cy.js
+++ Soifer/Tutorial_Cypress/cypress/e2e/each.cy.js
@@ -1,24 +1,24 @@
-describe('Search test cases',function(){
-    before(function(){
+describe('Search test cases',()=>{
+    before(()=>{
         cy.log("Ejecutando precondiciones a las pruebas")
     })
-    after(function(){
+    after(()=>{
         cy.log("Ejecutando postcondiciones a las pruebas")
     })
-    beforeEach(function(){ //Se ejecuta antes de cada prueba
+    beforeEach(()=>{ //Se ejecuta antes de cada prueba
         cy.visit('http://automationpractice.com/index.php')    
     })
-    afterEach(function(){ //Se ejecuta luego de cada prueba
+    afterEach(()=>{ //Se ejecuta luego de cada prueba
         cy.log('Poniendo datos en su estado inicial')
     })
     
-    it('Search dresses', function(){
+    it('Search dresses', ()=>{
         cy.get('#search_query_top').type('dress')
         cy.get('#searchbox > .btn').click()
         cy.get('.lighter').contains('"dress"')
     })
 
-    it('Search hats', function(){
+    it('Search hats', ()=>{
         cy.get('#search_query_top').type('hat')
         cy.get('#searchbox > .btn').click()
         cy.get('.lighter').contains('"hat"')
