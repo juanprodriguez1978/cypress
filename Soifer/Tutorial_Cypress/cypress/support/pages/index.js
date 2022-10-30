decd15ae52ac78d99a9dfc0d4d1346227afc868f
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2022-10-30 15:11:25 -0300

    add pages

---
+++ Soifer/Tutorial_Cypress/cypress/support/pages/index.js
@@ -0,0 +1,12 @@
+class indexPage{
+    constructor(){
+        this.searchInput = '#search_query_top'
+        this.searchButton = '#searchbox > .btn'
+    }
+
+    search = (element)=>{
+        cy.get(this.searchInput).type(element)
+        cy.get(this.searchButton).click()
+    }
+}
+export default new indexPage()
